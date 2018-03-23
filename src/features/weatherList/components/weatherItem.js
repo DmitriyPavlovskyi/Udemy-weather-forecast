ffefec1954f15f0f1f0297b71f9e2c324c3fb68d
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-23 17:22:20 +0200

    Sperklines graphics added

--- src/features/weatherList/components/weatherItem.js
+++ src/features/weatherList/components/weatherItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 function WeatherItem(props) {
   const name = props.data.city.name;
+  const temps = props.data.list.map(weather => weather.main.temp);
 
   return (
     <tr>
+      <td>{name}</td>
       <td>
-        {name}
+        <Sparklines height={150} width={300} data={temps}>
+          <SparklinesLine color="blue"/>
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
       </td>
+
     </tr>
   );
 }
